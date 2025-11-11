afb684c360d73d12f45b28797e59f546692d1364
Cheng <mazeyqian@gmail.com>
2025-11-11 13:22:22 +0800

    feat(scripts): Find all following users who did not follow me

---
+++ scripts/duolingo/calc-mutuals.js
@@ -0,0 +1,21 @@
+// https://www.duolingo.cn/profile/CHENG413
+
+const following = require('./following.json');
+const followers = require('./followers.json');
+
+const nonMutualFollowing = following.filter(user => {
+    return !followers.some(follower => follower.userId === user.userId);
+});
+
+const mutualFollowing = following.filter(user => {
+    return followers.some(follower => follower.userId === user.userId);
+});
+
+console.log(`Number of people I follow: ${following.length}`);
+console.log(`Number of followers: ${followers.length}`);
+console.log(`Number of non-mutual follows: ${nonMutualFollowing.length}`);
+console.log(`Number of mutual follows: ${mutualFollowing.length}`);
+
+nonMutualFollowing.forEach(user => {
+    console.log(`Username: ${user.username}, Displayname: ${user.displayName}`);
+});

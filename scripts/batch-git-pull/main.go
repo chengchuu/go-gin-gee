b04336b06c4d3927b982e2848a35f8bcfeede5cc
Cheng <mazeyqian@gmail.com>
2026-02-04 16:29:54 +0800

    feat(scripts): revert comments

--- scripts/batch-git-pull/main.go
+++ scripts/batch-git-pull/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os/exec"
 	"regexp"
+	"runtime"
 
 	"github.com/bitfield/script"
 	"github.com/chengchuu/go-gin-gee/internal/pkg/constants"
@@ -40,41 +41,41 @@ func main() {
 	// Example: ^.+(placeholder|.)$
 	regexStr += fmt.Sprintf("%s)\\/\\.git$", *assignedProjects)
 	regex := regexp.MustCompile(regexStr)
-	// if runtime.GOOS == "windows" {
-	// 	script.ListFiles(fmt.Sprintf("%s\\*\\.git", *projectPath)).FilterLine(func(s string) string {
-	// 		cmdLines := constants.ScriptStartMsgInWin + " && "
-	// 		cmdLines += fmt.Sprintf("echo Path: %s && ", s)
-	// 		// https://stackoverflow.com/questions/607670/windows-shell-command-to-get-the-full-path-to-the-current-directory
-	// 		cmdLines += fmt.Sprintf("cd %s && ", s)
-	// 		cmdLines += `cd ../ && `
-	// 		cmdLines += `git pull && `
-	// 		cmdLines += "echo All done in Windows CMD. && "
-	// 		cmdLines += constants.ScriptEndMsgInWin
-	// 		cmd := exec.Command("cmd", "/C", cmdLines)
-	// 		result, err := cmd.CombinedOutput()
-	// 		if err != nil {
-	// 			logger.Println("error:", err)
-	// 		}
-	// 		logger.Printf("result: %s", result)
-	// 		return ""
-	// 	}).Stdout()
-	// } else {
-	script.ListFiles(fmt.Sprintf("%s/*/.git", *projectPath)).MatchRegexp(regex).FilterLine(func(s string) string {
-		cmdLines := constants.ScriptStartMsg
-		cmdLines += fmt.Sprintf("echo Path: %s;", s)
-		cmdLines += fmt.Sprintf("cd %s;", s)
-		// Control the branch: cmdLines += `git checkout master;`
-		cmdLines += `cd ../;`
-		cmdLines += *runCommands
-		cmdLines += constants.ScriptEndMsg
-		cmd := exec.Command("/bin/sh", "-c", cmdLines)
-		result, err := cmd.CombinedOutput()
-		if err != nil {
-			logger.Println("error:", err)
-		}
-		logger.Printf("result: %s", result)
-		return ""
-	}).Stdout()
-	// }
+	if runtime.GOOS == "windows" {
+		script.ListFiles(fmt.Sprintf("%s\\*\\.git", *projectPath)).FilterLine(func(s string) string {
+			cmdLines := constants.ScriptStartMsgInWin + " && "
+			cmdLines += fmt.Sprintf("echo Path: %s && ", s)
+			// https://stackoverflow.com/questions/607670/windows-shell-command-to-get-the-full-path-to-the-current-directory
+			cmdLines += fmt.Sprintf("cd %s && ", s)
+			cmdLines += `cd ../ && `
+			cmdLines += `git pull && `
+			cmdLines += "echo All done in Windows CMD. && "
+			cmdLines += constants.ScriptEndMsgInWin
+			cmd := exec.Command("cmd", "/C", cmdLines)
+			result, err := cmd.CombinedOutput()
+			if err != nil {
+				logger.Println("error:", err)
+			}
+			logger.Printf("result: %s", result)
+			return ""
+		}).Stdout()
+	} else {
+		script.ListFiles(fmt.Sprintf("%s/*/.git", *projectPath)).MatchRegexp(regex).FilterLine(func(s string) string {
+			cmdLines := constants.ScriptStartMsg
+			cmdLines += fmt.Sprintf("echo Path: %s;", s)
+			cmdLines += fmt.Sprintf("cd %s;", s)
+			// Control the branch: cmdLines += `git checkout master;`
+			cmdLines += `cd ../;`
+			cmdLines += *runCommands
+			cmdLines += constants.ScriptEndMsg
+			cmd := exec.Command("/bin/sh", "-c", cmdLines)
+			result, err := cmd.CombinedOutput()
+			if err != nil {
+				logger.Println("error:", err)
+			}
+			logger.Printf("result: %s", result)
+			return ""
+		}).Stdout()
+	}
 	logger.Println("Git Pull Done.")
 }

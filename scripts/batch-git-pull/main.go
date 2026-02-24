f5e1cd7cba496613715c4d0243297fcc03e8b58e
Cheng <mazeyqian@gmail.com>
2026-02-24 17:01:08 +0800

    feat(scripts): remove param, projects

--- scripts/batch-git-pull/main.go
+++ scripts/batch-git-pull/main.go
@@ -5,46 +5,32 @@ import (
 	"fmt"
 	"os/exec"
 	"path/filepath"
-	"regexp"
 	"runtime"
 	"strings"
 
 	"github.com/bitfield/script"
-	"github.com/chengchuu/go-gin-gee/internal/pkg/constants"
 	"github.com/chengchuu/go-gin-gee/pkg/logger"
 )
 
 // Examples:
 // go run scripts/batch-git-pull/main.go -path="/Users/Web/web"
 // go run scripts/batch-git-pull/main.go -path="C:\Web\web"
-// go run scripts/batch-git-pull/main.go -path="C:\Web\web" -projects="placeholder1|placeholder2"
 // path required
-// projects optional
 func main() {
 	logger.Println("Git Pull ...")
 	placeholder := "unknown"
+
 	// https://gobyexample.com/command-line-flags
 	projectPath := flag.String("path", placeholder, "folder of projects")
-	assignedProjects := flag.String("projects", ".", "assigned projects (regex)")
 	runCommands := flag.String("commands", "git pull", "commands")
 	flag.Parse()
+
 	logger.Println("projectPath:", *projectPath)
-	logger.Println("assignedProjects:", *assignedProjects)
 	logger.Println("runCommands:", *runCommands)
+
 	if *projectPath == placeholder {
 		logger.Fatal("path is required")
 	}
-	projects := []string{
-		"placeholder",
-	}
-	regexStr := "^.+("
-	for _, v := range projects {
-		regexStr += fmt.Sprintf("%s|", v)
-	}
-	// Example: ^.+(placeholder|.)$
-	regexStr += fmt.Sprintf("%s)\\/\\.git$", *assignedProjects)
-	logger.Info("regexStr: %s", regexStr)
-	regex := regexp.MustCompile(regexStr)
 
 	// helper to check if repo has a remote configured
 	hasRemote := func(repoDir string) (string, error) {
@@ -52,7 +38,6 @@ func main() {
 		cmd := exec.Command("git", "-C", repoDir, "config", "--get", "remote.origin.url")
 		out, err := cmd.Output()
 		if err != nil {
-			// return empty string and error
 			return "", err
 		}
 		return strings.TrimSpace(string(out)), nil
@@ -60,7 +45,7 @@ func main() {
 
 	if runtime.GOOS == "windows" {
 		// Windows: use PowerShell; iterate .git folders under projectPath
-		script.ListFiles(fmt.Sprintf("%s\\*\\.git", *projectPath)).FilterLine(func(s string) string {
+		script.ListFiles(fmt.Sprintf(`%s\*\.git`, *projectPath)).FilterLine(func(s string) string {
 			// compute repo dir (parent of .git)
 			repoDir := filepath.Dir(s)
 			logger.Info("found repo: %s", repoDir)
@@ -71,8 +56,8 @@ func main() {
 				return ""
 			}
 			logger.Info("remote for repo: %s", remoteURL)
+
 			// Build PowerShell command lines; quote path to handle spaces
-			// Using the provided runCommands (which may contain multiple commands)
 			cmdLines := fmt.Sprintf("Write-Output 'Path: %s'; ", repoDir)
 			cmdLines += fmt.Sprintf("Set-Location -LiteralPath '%s'; ", repoDir)
 			cmdLines += fmt.Sprintf("%s; ", *runCommands)
@@ -87,21 +72,33 @@ func main() {
 			return ""
 		}).Stdout()
 	} else {
-		script.ListFiles(fmt.Sprintf("%s/*/.git", *projectPath)).MatchRegexp(regex).FilterLine(func(s string) string {
-			cmdLines := constants.ScriptStartMsg
-			cmdLines += fmt.Sprintf("echo Path: %s;", s)
-			cmdLines += fmt.Sprintf("cd %s;", s)
-			cmdLines += `cd ../;`
+		// Unix-like: iterate .git folders under projectPath
+		script.ListFiles(fmt.Sprintf("%s/*/.git", *projectPath)).FilterLine(func(s string) string {
+			repoDir := filepath.Dir(s)
+			logger.Info("found repo: %s", repoDir)
+
+			remoteURL, err := hasRemote(repoDir)
+			if err != nil || remoteURL == "" {
+				logger.Info("no remote found for %s\n-- Skipping --", repoDir)
+				return ""
+			}
+			logger.Info("remote for repo: %s", remoteURL)
+
+			cmdLines := fmt.Sprintf("echo Path: %s;", repoDir)
+			// quote repoDir to handle spaces
+			cmdLines += fmt.Sprintf("cd '%s';", repoDir)
 			cmdLines += fmt.Sprintf("%s;", *runCommands)
-			cmdLines += constants.ScriptEndMsg
+			cmdLines += "echo '-- All Done in Shell --';"
+
 			cmd := exec.Command("/bin/sh", "-c", cmdLines)
 			result, err := cmd.CombinedOutput()
 			if err != nil {
-				logger.Println("error:", err)
+				logger.Error("error running commands in %s: %v", repoDir, err)
 			}
-			logger.Printf("result: %s", result)
+			logger.Info("result:\n%s", result)
 			return ""
 		}).Stdout()
 	}
+
 	logger.Println("Git Pull Done.")
 }

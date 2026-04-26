cb0201f916a1352257dba793f54b478b26e3e65f
Cheng <mazeyqian@gmail.com>
2026-04-26 22:26:14 +0800

    feat(scripts): convert all Git remotes from HTTPS to SSH

---
+++ scripts/batch-convert-https-to-ssh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# batch-convert-https-to-ssh.sh
+
+ROOT="$HOME/dev"   # change this
+find "$ROOT" -type d -name .git -prune | while read -r gitdir; do
+  repo="${gitdir%/.git}"
+  echo "==> $repo"
+  git -C "$repo" remote -v | awk '{print $1}' | sort -u | while read -r r; do
+    url="$(git -C "$repo" remote get-url "$r" 2>/dev/null || true)"
+    case "$url" in
+      https://github.com/*)
+        new="git@github.com:${url#https://github.com/}"
+        git -C "$repo" remote set-url "$r" "$new"
+        echo "  $r: $url -> $new"
+        ;;
+      *)
+        echo "  $r: skip ($url)"
+        ;;
+    esac
+  done
+done

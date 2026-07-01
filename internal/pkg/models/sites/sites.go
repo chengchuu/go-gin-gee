44173efa93b2bf4ae2e6042c3e3e8cdfa87f5e98
Cheng <mazeyqian@gmail.com>
2026-07-01 20:05:48 +0800

    feat(model): Cleaner version for website

--- internal/pkg/models/sites/sites.go
+++ internal/pkg/models/sites/sites.go
@@ -1,7 +1,7 @@
 package models
 
 type WebSite struct {
-	Name string `gorm:"column:name;type:varchar(100);not null" json:"name"`
-	Link string `gorm:"column:link;type:varchar(500);not null" json:"link"`
-	Code int    `gorm:"column:code;type:smallint;not null;default:200" json:"code"`
+	Name string `json:"name"`
+	Link string `json:"link"`
+	Code int    `json:"code"`
 }

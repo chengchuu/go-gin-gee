d851a2759a27e3e592d0737e49d64f880c01cdbb
Cheng <mazeyqian@gmail.com>
2026-07-01 20:03:49 +0800

    feat(model): optimise varchar size

--- internal/pkg/models/sites/sites.go
+++ internal/pkg/models/sites/sites.go
@@ -2,6 +2,6 @@ package models
 
 type WebSite struct {
 	Name string `gorm:"column:name;type:varchar(100);not null" json:"name"`
-	Link string `gorm:"column:link;type:varchar(2048);not null" json:"link"`
+	Link string `gorm:"column:link;type:varchar(500);not null" json:"link"`
 	Code int    `gorm:"column:code;type:smallint;not null;default:200" json:"code"`
 }
